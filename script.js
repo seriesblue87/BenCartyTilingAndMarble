b782c8f1396d4d6eb383125597263f3167f0b1c5
seriesblue87 <brandon.renschler@gmail.com>
2026-04-22 14:54:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,24 @@ function toggleMenu() {
 // active nav highlight
 const links = document.querySelectorAll("nav a");
 
-// get current page (fixes index.html + subpages)
-const currentPage = window.location.pathname.split("/").pop() || "index.html";
+// get current page safely
+let currentPage = window.location.pathname.split("/").pop();
+
+// fix homepage case
+if (currentPage === "" || currentPage === "/") {
+  currentPage = "index.html";
+}
 
 links.forEach(link => {
   const linkPage = link.getAttribute("href");
 
   // highlight active page
-  if (
-    linkPage === currentPage ||
-    (currentPage === "" && linkPage === "index.html")
-  ) {
+  if (linkPage === currentPage) {
     link.classList.add("active");
   }
+
+  // close mobile menu on click
+  link.addEventListener("click", () => {
+    document.getElementById("navMenu").classList.remove("show");
+  });
 });
