d9277819cf575803e411fa6681c431d144b2ad2d
seriesblue87 <brandon.renschler@gmail.com>
2026-04-22 14:43:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ function toggleMenu() {
 // active nav highlight
 const links = document.querySelectorAll("nav a");
 
+// get current page (fixes index.html + subpages)
+const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
 links.forEach(link => {
-  if (link.href === window.location.href) {
+  const linkPage = link.getAttribute("href");
+
+  // highlight active page
+  if (
+    linkPage === currentPage ||
+    (currentPage === "" && linkPage === "index.html")
+  ) {
     link.classList.add("active");
   }
 });
