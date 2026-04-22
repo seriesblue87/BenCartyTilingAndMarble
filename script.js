fd0f4f6822376eb7e5b955e6117bc2416b923168
seriesblue87 <brandon.renschler@gmail.com>
2026-04-22 13:55:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// hamburger toggle
 function toggleMenu() {
   document.getElementById("navMenu").classList.toggle("show");
 }
+
+// highlight active page WITHOUT shifting
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+  if (link.href === window.location.href) {
+    link.classList.add("active");
+  }
+});
